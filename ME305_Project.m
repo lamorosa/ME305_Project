4c2c6c017ef16b446ef919e099b7a0d97a00abd4
kpdudek <kpdudek2@gmail.com>
2018-12-04 20:32:38 -0500

    Ready to merge with master and accept Lucas changes

--- ME305_Project.m
+++ ME305_Project.m
@@ -1,3 +1,14 @@
+%%% ---   ME305 Beam Design Project   --- %%%
+% Kurt Dudek & Luca Amarosa
+% 2018/12/07
+
+clear
+clc
+
+% Test case
+% materials = struct('Material','AL','G',2.7*10^10,'E',7.1*10^10,'Density',2780,'Y',315*10^6);
+% materials(2) = struct('Material','Steel','G',3.1*10^10,'E',9.5*10^10,'Density',4300,'Y',415*10^6);
+
 %Defining given constants
 P=30000;
 T=2500;
@@ -18,41 +29,45 @@ ri2=[in2m(.75:.125:1.15)];
 % Struct with best performing configuration
 optimal_beam = struct('Material','','Weight',100000000,'ro1',1.5,'ri1',0,'ro2',1.25,'ri2',0);
 
+% Store all data from test
+for mats = 1:6
+    data(mats) = struct('Material','','Weight',[],'ro1',1.5,'ri1',[],'ro2',1.25,'ri2',[],'twist',[],...
+        'delta',[],'effect_stress',[]);
+end
+
+loop_val = 1;
 % Loop through bar geometries and materials to find best performance
-for i = 1:length(ri1)
-    for j = 1:length(ri2)
-        for k = 1:length(materials)
+for k = 1:length(materials)
+    for i = 1:length(ri1)
+        for j = 1:length(ri2)
             [weight,twist,delta,effect_stress] = bar_params(ro1,ri1(i),ro2,ri2(j),P,T,materials(k).G,...
-                materials(k).E,L1,L2,materials(k).density);
+                materials(k).E,L1,L2,materials(k).Density);
+            data(k).Material = materials(k).Material;
+            data(k).Weight(end+1) = weight;
+            data(k).twist(end+1) = twist;
+            data(k).delta(end+1) = delta;
+            data(k).effect_stress(end+1) = effect_stress;
+            data(k).ri1(end+1) = ri1(i);
+            data(k).ri2(end+1) = ri2(j);
             
             % If bar configuration passes strength requirements
-            if (twist<(twist_max./2)) && (delta<(delta_tot./2)) && (effect_stress<(stress_max./2))
+            if (twist<(twist_max./2)) && (delta<(delta_tot./2)) && (effect_stress<(materials(k).Y./2))
                 % Write current bar configurations to the struct of optimal design
-                if weight < optimal beam.Weight
-                    optimal_beam.Material = material(k).Material;
+                if weight < optimal_beam.Weight
+                    optimal_beam.Material = materials(k).Material;
                     optimal_beam.Weight = weight;
-                    optimal_beam.ri1 = material(k).ri1;
-                    optimal_beam.ri2 = material(k).ri2;
+                    optimal_beam.ri1 = ri1(i);
+                    optimal_beam.ri2 = ri2(j);
+                    iter = i * j * k;
+                    fprintf('Optimal Updated with %s, iteration %d...\n',materials(k).Material,iter)
+                else
+                    fprintf('%s, iteration %d, Passed but not optimal...\n',materials(k).Material,iter)
                 end
-                iter = i * j * k;
-                fprintf('Optimal Updated with %s, iteration %d',material(k).Material,iter)
             else
-                fprintf('Failed...')
+                fprintf('Failed; Try harder...\n')
             end
+            disp(loop_val)
+            loop_val = loop_val+1;
         end
     end
 end
-
-
-
-
-
-% %Material specific calculations
-% G=27*10^9;
-% E=71.*10^9;
-% density=[2780];
-% stress_max=315*10^6;
-
-% save('Variables_AL-7016-T5_Test1.mat','G','E','density','stress_max','ro1','ri1','ro2','ri2')
-% 
-% disp('Results for AL-7016-T5, Test1')
--- in2m.m
+++ in2m.m
@@ -1,5 +1,5 @@
 function [m] = in2m(in)
+% Convert inches to meters
 m=in.*0.0254;
-
 end
 
