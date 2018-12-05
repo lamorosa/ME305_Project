037b5333d515a63a154f9e4221b6ffcc21dcc499
kpdudek <kpdudek2@gmail.com>
2018-12-04 19:06:18 -0500

    Making changes to loop through mats

--- ME305_Project.m
+++ ME305_Project.m
@@ -15,27 +15,32 @@ E=71.*10^9;
 density=[2780];
 stress_max=315*10^6;
 
-ro1=[in2m(1.5)];
-ri1=[in2m(1.35)];
+ro1=[in2m(.75:.125:2)];
+ri1=[in2m(.625:.125:1.875)];
 
-ro2=[in2m(1.25)];
-ri2=[in2m(1.10)];
+ro2=[in2m(.5:.125:1.75)];
+ri2=[in2m(.375:.125:1.625)];
+
+
+[weight, twist, delta, effect_stress] = bar_params(ro1,ri1,ro2,ri2,P,T,G,E,L1,L2,density)
+
+
+
+
+
+
+
+
+
 
 save('Variables_AL-7016-T5_Test1.mat','G','E','density','stress_max','ro1','ri1','ro2','ri2')
 
 disp('Results for AL-7016-T5, Test1')
 
-[weight, twist, delta, effect_stress] = bar_params(ro1,ri1,ro2,ri2,P,T,G,E,L1,L2,density);
-
-
 if twist<(twist_max./2)
     disp('Twist passed')
 else
-<<<<<<< HEAD
-    disp('fail')
-=======
     disp('Twist failed')
->>>>>>> c5a44bdfa3d35a5d603e3ea3df176c11fd4c7d68
 end
 
 if delta<(delta_tot./2)
