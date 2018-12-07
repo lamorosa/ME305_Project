6c5ece4f6f6260573bbaf61edc90dba7d9de5b0f
kpdudek <kpdudek2@gmail.com>
2018-12-07 01:36:42 -0500

    Final Design

--- bar_params.m
+++ bar_params.m
@@ -2,7 +2,7 @@ function [weight, twist, delta, effect_stress] = bar_params(ro1,ri1,ro2,ri2,P,T,
 % Compute the area of the straight portion
 A2=pi.*(ro2.^2-ri2.^2);
 % Compute the polar moment of inertia
-Ip2=(pi/2)*(ro2.^4-ri2.^4);
+Ip2=(pi/2).*(ro2.^4-ri2.^4);
 
 % Values for integrating, the linear slopes of the inner and outer wall
 a=(ro2-ro1)/L1;
@@ -25,7 +25,7 @@ delta=delta_1+delta_2;
 % Superposition for the stress values
 norm_stress_x = P/A2;
 shear_stress_yz=(T.*ro2)/Ip2;
-effect_stress = (1/sqrt(2)).*sqrt(2.*(norm_stress_x.^2)+6.*(shear_stress_yz^.2));
+effect_stress = (1/sqrt(2)).*sqrt(2.*(norm_stress_x.^2)+6.*(shear_stress_yz.^2));
 
 % Compute the volume integral and corresponding weight
 syms x
